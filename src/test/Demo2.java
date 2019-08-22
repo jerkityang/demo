a2d16e824280726a7fe5d54224826f9a2547e7db
jerkityang <13189806060@163.com>
2019-08-22 10:36:20 +0800

    1

--- src/test/Demo2.java
+++ src/test/Demo2.java
@@ -4,5 +4,8 @@ public class Demo2 {
 
 	public static void main(String[] args) {
 		System.out.println("分支测试1");
+		System.out.println("分支测试1");
+		System.out.println("分支测试1");
+		System.out.println("分支测试1");
 	}
 }
